cb1e01ccfe09aa977c508efa59399b9ca60657f9
Mark Rybin <snabbycrab@gmail.com>
2021-09-15 21:40:56 +0300

    päivitys

--- src/main/java/hh/swd20/kirjakauppa/domain/Book.java
+++ src/main/java/hh/swd20/kirjakauppa/domain/Book.java
@@ -1,8 +1,48 @@
 package hh.swd20.kirjakauppa.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Book {
-	// attribuutit
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private String title, author, isbn;
-	private int year;
-	private double price;
+	private double year, price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getYear() {
+		return year;
+	}
+	public void setYear(double year) {
+		this.year = year;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	
+	
 }
